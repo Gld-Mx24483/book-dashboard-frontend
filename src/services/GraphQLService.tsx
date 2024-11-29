96af01ddd07ca0e95a727886ab9288b357f95528
Gld_Mx24483 <israeltoki7@gmail.com>
2024-11-29 17:30:34 +0100

    build: :rocket: Deployment
    Deploy frontend

--- src/services/GraphQLService.tsx
+++ src/services/GraphQLService.tsx
@@ -12,8 +12,17 @@ import React from 'react';
 const GraphQLProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
 
+  // const httpLink = createHttpLink({
+  //   uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || 'http://localhost:9000/graphql',
+  // });
+
+  const graphqlEndpoint = 
+    process.env.NODE_ENV === 'production' 
+      ? process.env.REACT_APP_PRODUCTION_BACKEND_URL 
+      : process.env.REACT_APP_LOCAL_BACKEND_URL || 'http://localhost:9000/graphql';
+
   const httpLink = createHttpLink({
-    uri: process.env.REACT_APP_GRAPHQL_ENDPOINT || 'http://localhost:9000/graphql',
+    uri: graphqlEndpoint,
   });
 
   const authLink = setContext(async () => {
